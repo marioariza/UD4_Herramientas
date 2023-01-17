e5217ab2a8f2a3197042c9a3fc9c9d4248dac7de
Mario Rufo Ariza <mariojra6@gmail.com>
2023-01-17 23:48:55 +0100

    Archivos actualizados

--- src/Cliente.php
+++ src/Cliente.php
@@ -4,13 +4,14 @@ class Cliente {
     public string $nombre;
     private int $numero;
     private array $dulcesComprados = [];
-    private int $numDulcesComprados;
+    private int $numDulcesComprados = 0;
     private int $numPedidosEfectuados;
 
-    public function __construct($nombre, $numero)
+    public function __construct($nombre, $numero, $numPedidosEfectuados = 0)
     {
         $this->nombre = $nombre;
         $this->numero = $numero;
+        $this->numPedidosEfectuados = $numPedidosEfectuados;
     }
 
     public function getNombre()
@@ -37,6 +38,18 @@ class Cliente {
         return $this;
     }
 
+    public function getNumPedidosEfectuados()
+    {
+        return $this->numPedidosEfectuados;
+    }
+
+    public function setNumPedidosEfectuados($numPedidosEfectuados)
+    {
+        $this->numPedidosEfectuados = $numPedidosEfectuados;
+
+        return $this;
+    }
+
     public function comprar(Dulces $d) {
         if ($this->listaDeDulces($d) == true) {
             $this->numDulcesComprados++;
@@ -52,7 +65,13 @@ class Cliente {
     }
 
     public function valorar(Dulces $d, string $c) {
-        
+        if ($this->listaDeDulces($d) == true) {
+            echo "Gracias por dejar tu valoración " . $this->nombre . ", la usaremos para mejorar.
+            <br><br>
+            <b>Valoración: </b>" . $c;
+        } else {
+            echo "No puedes realizar la valoración de un dulce que no ha sido comprado";
+        }
     }
 
     public function listaDeDulces(Dulces $d) : bool{
@@ -68,10 +87,10 @@ class Cliente {
     }
 
     public function muestraResumen() {
-        echo '<b>Resumen cliente:</b><br>---------------------<br>' . '<b>Nombre = </b>' . $this->nombre .
+        echo '<b>Resumen cliente:</b><br>********************<br>' . '<b>Nombre = </b>' . $this->nombre .
             '<br><b>Número = </b>' . $this->numero;
         foreach ($this->dulcesComprados as $dulc) {
-            echo '<br><b>Relleno = </b>'.$dulc->getNombre;
+            echo '<br><b>Dulce comprado = </b>'.$dulc->getNombre();
         }
         echo '<br><b>Número de dulces comprados = </b>'.$this->numDulcesComprados.
         '<br><b>Número de pedidos realizados = </b>'.$this->numPedidosEfectuados;
--- src/index5.php
+++ src/index5.php
@@ -2,9 +2,27 @@
 
 include "Cliente.php";
 include "Dulces.php";
+include "Tarta.php";
+include "Bollo.php";
 
+$cliente1 = new Cliente("Mario", 7);
+
+$bollo1 = new Bollo("Cuña de chocolate", 15, 1.5, "Crema");
 $tarta1 = new Tarta("Tarta tres chocolates", 34, 4, ["Chocolate negro, Chocolate con leche, Chocolate blanco"], 3, 1, 1);
+$tarta2 = new Tarta("Tarta de la abuela", 33, 4, ["Chocolate, Natilla, Galleta"], 3, 1, 1);
 
-$tarta1->muestraResumen();
+$cliente1->comprar($tarta1);
+echo "<br><br>-----------------------------------------<br><br>";
+$cliente1->comprar($tarta1);
+echo "<br><br>-----------------------------------------<br><br>";
+$cliente1->comprar($bollo1);
+echo "<br><br>-----------------------------------------<br><br>";
+$cliente1->valorar($tarta1, "La mejor tarta del mundo :)");
+echo "<br><br>-----------------------------------------<br><br>";
+$cliente1->valorar($tarta2, "Tarta buena pero las hay mejores");
+echo "<br><br>-----------------------------------------<br><br>";
+$cliente1->muestraResumen();
+
+
 
 ?>
