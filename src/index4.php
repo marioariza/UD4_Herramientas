4b3165b6411276df14e12dbabb0969c7127da54e
*Mario <*mariojra6@gmail.com*>
2023-01-17 12:27:37 +0100

    Archivos actualizados

--- src/index4.php
+++ src/index4.php
@@ -2,7 +2,7 @@
 
 include "Tarta.php";
 
-$tarta1 = new Tarta("Tarta tres chocolates", 34, 4, ["Chocolate negro, Chocolate con leche, Chocolate blanco"], 3, 1);
+$tarta1 = new Tarta("Tarta tres chocolates", 34, 4, ["Chocolate negro, Chocolate con leche, Chocolate blanco"], 3, 4, 1);
 
 $tarta1->muestraResumen();
 
