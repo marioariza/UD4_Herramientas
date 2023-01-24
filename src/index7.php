a6656972403dfef11ba916fd73b039d404fda453
Mario Rufo Ariza <mariojra6@gmail.com>
2023-01-24 18:57:47 +0100

    Archivos actualizados

--- src/index7.php
+++ src/index7.php
@@ -10,3 +10,9 @@ include "Chocolate.php";
 $pasteleria = new Pasteleria("Confitería la Campana");
 
 $pasteleria->incluirTarta("Tarta tres chocolates", 34, 4, ["Chocolate negro, Chocolate con leche, Chocolate blanco"], 3, 1, 1);
+
+$pasteleria->listarProductos();
+
+$chocolate1 = new Chocolate("Palmera de chocolate", 3, 1.8, 60, 165);
+
+$chocolate1->setPorcentajeCacao(70);
--- tests/TestPasteleria.php
+++ tests/TestPasteleria.php
@@ -18,7 +18,11 @@ class TestPasteleria extends TestCase
         $pasteleria = new Pasteleria("Pastelería Los Hermanos Moyazo");
         $pasteleria->incluirTarta("Tarta tres chocolates", 34, 4, ["Chocolate negro, Chocolate con leche, Chocolate blanco"], 3, 1, 1);
 
-        $this->expectOutputString("INCLUIDO PRODUCTO 1 <br>****************************<br><b>Nombre = </b> Tarta tres chocolates");
+        $this->expectOutputString('<b>Resumen dulce:</b><br>********************<br><b>Nombre = </b>Tarta tres chocolates<br><b>Número = </b>34
+        <br><b>Precio = </b>4 €<br><b>Precio con IVA = </b>4.84 €<br><b>Relleno = </b>Chocolate negro, Chocolate con leche, Chocolate blanco
+        <br><b>Número de pisos = </b>3<br><b>Comensales = </b>Para 1 comensal.');
         $pasteleria->listarProductos();
     }
 }
+
+?>
