89dba267b0a77a9256f36e92bc103b0fd1e10771
*Mario <*mariojra6@gmail.com*>
2023-01-24 09:58:43 +0100

    Archivos actualizados

--- src/index1.php
+++ src/index1.php
@@ -2,8 +2,12 @@
 
 include "Dulces.php";
 
+echo "Da error al instanciar la clase Dulces ya que ahora esta clase es abstracta.<br><br>";
+
 $dulce1 = new Dulces("Donut", 22, 2);
 
 $dulce1->muestraResumen();
+
+// Da error al instanciar la clase Dulces ya que ahora esta clase es abstracta.
 
 ?>
