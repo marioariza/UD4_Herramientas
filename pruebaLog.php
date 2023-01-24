83a14c9ae9d50a3d04e3606b004ebbdcbf732b20
*Mario <*mariojra6@gmail.com*>
2023-01-24 13:35:44 +0100

    Archivos actualizados

--- pruebaLog.php
+++ pruebaLog.php
@@ -14,3 +14,6 @@ $log->warning("Esto es un mensaje de WARNING");
 $log->error("Esto es un mensaje de ERROR");
 $log->critical("Esto es un mensaje de CRITICAL");
 $log->alert("Esto es un mensaje de ALERT");
+
+$log->warning("Producto no encontrado", [$producto]);
+$log->warning("Producto no encontrado", ["datos" => $producto]);
